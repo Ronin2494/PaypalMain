e433ee5a87f4a5d5e05c2d1ca10288fb561250c5
Ronin2494 <deepaksharma2494@gmail.com>
2020-12-04 10:13:17 -0500

    Initial commit

---
+++ app/src/main/java/com/example/paypal/Config/Config.java
@@ -0,0 +1,7 @@
+package com.example.paypal.Config;
+
+public class Config {
+
+    public static final String PAYPAL_CLIENT_ID = "AaRA3cu1lDT_nEUznn0lhM2HJ91paKHJATFvZJG6YpdELCLRSI7fp97-uEGUYx3px1ZFgKixzZuVUgdm";
+
+}
